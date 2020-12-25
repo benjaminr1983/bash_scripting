26cc2323e4e591388d0bd768902b1b9936ea1e87
benjaminr1983 <benjaminreinicke@tuta.io>
2020-12-25 15:52:53 +0100

    geändert:       git_push.sh

--- git_push.sh
+++ git_push.sh
@@ -1,47 +1,35 @@
 #!/bin/bash
 # request the variables
+git_user=$(git config --list | grep user.name | sed 's/.*[=] *//')
 echo
 read -p "Please enter the local folder for the repository:  " local_repo_directory && echo
 # cd ~/Github/$local_repo_directory
-read -p "Please enter your username in git:  " git_user && echo
-while true;
-do
-    read -p "Do you use a passphrase with your private key? [y/n]:  " yn && echo
-    case $yn in
-	[Yy]* ) read -s -p "Please enter the password of your private key:  " ssh_pwd && echo;
-		export $ssh_pwd;
-		break;;
-	[Nn]* ) break;;
-	* ) echo You entered an invalid answer, please only use y,Y,n,N
-    esac
-done
-echo
 read -p "Please enter the repository:  " repo_name && echo
 while true;
 do
     read -p "Do you want to create a new repository, switch an older repository to ssh or simply push changes in the current repository? [c/s/p]:  " csp && echo
-    case $csp in
-	# new repository
-	[Cc]* ) echo "# $repo_name" >> README.md;
-		cp -p ~/Github/LICENSE.md ~/Github/$local_repo_directory/LICENSE;
-		git init;
-		git add .;
-		git commit -a;
-		git branch -M main;
-		git remote add origin git@github.com:$git_user/$repo_name.git;
-		break;;
-	# update existing repository
-	[Ss]* ) git commit -a;
-		git remote set-url origin git@github.com:$git_user/$repo_name.git;
-		break;;
-	# push updates
-	[Pp]* ) git commit -a;
-		break;;
-	* ) echo You entered an invalid answer, please only use N, n, S, s, P, p
-    esac
+case $csp in
+    # new repository
+    [Cc]* ) echo "# $repo_name" >> README.md;
+	    cp -p ~/Github/LICENSE.md ~/Github/$local_repo_directory/LICENSE;
+	    git init;
+	    git add .;
+	    git commit -a;
+	    git branch -M main;
+	    git remote add origin git@github.com:$git_user/$repo_name.git;
+	    break;;
+    # update existing repository
+    [Ss]* ) git commit -a;
+	    git remote set-url origin git@github.com:$git_user/$repo_name.git;
+	    break;;
+    # push updates
+    [Pp]* ) git commit -a;
+	    break;;
+    * ) echo You entered an invalid answer, please only use N, n, S, s, P, p
+esac
 done
 # add the key to ssh-agent
 eval "$(ssh-agent -s)"
-$ssh_pwd -e ssh-add ~/.ssh/github
+ssh-add ~/.ssh/github
 # push the changes to git
 git push -u origin main
