7e4f4fe8acac57ea0a2895905d8571871955a261
benjaminr1983 <benjaminreinicke@tuta.io>
2021-12-25 10:26:44 +0100

    modified:   01_templates/02_bash_script.sh new file:   backup_script.sh 	new file:   config_backup.sh 	new file:   renameing.sh 	modified:   xfce_manager

--- config_backup.sh
+++ config_backup.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 #--------------------------------------------------------------------------------------------------------------------------------------------#
-# script_name: 
+# script_name: config_backup.sh
 # script_version: 0.1
 # script_author: Benjamin Reinicke
 # script_date: ..2021
 # script_license: GPL-2.0
 #--------------------------------------------------------------------------------------------------------------------------------------------#
+for f in $HOME/.bash*
+    do 
+        cp "$f" "$HOME/git_hub_projects/05_config_files/bash"
+    done
+
+
